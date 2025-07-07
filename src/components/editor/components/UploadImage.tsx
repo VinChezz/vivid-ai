c6a859634d59f03e4be71fcf43e329d4b050358a
VinChezz <vin4auzer@gmail.com>
2025-07-07 23:00:13 +0300

    fix(UploadImage): install new module @uploadcare/react-uploader

--- src/components/editor/components/UploadImage.tsx
+++ src/components/editor/components/UploadImage.tsx
@@ -1,32 +1,34 @@
-"use client";
-import { FileUploaderRegular, } from "@uploadcare/react-uploader/next";
-import "@uploadcare/react-uploader/core.css";
+'use client';
+import { FileUploaderRegular } from '@uploadcare/react-uploader/next';
+import '@uploadcare/react-uploader/core.css';
 
 type Props = {
-  contentId: string;
-  onContentChange: (
-    contentId: string,
-    newContent: string | string[] | string[][]
-  ) => void;
+    contentId: string;
+    onContentChange: (
+        contentId: string,
+        newContent: string | string[] | string[][],
+    ) => void;
 };
 
 function UploadImage({ contentId, onContentChange }: Props) {
-  const handleChangeEvent = (e: { cdnUrl: string | string[] | string[][]; }) => {
-    onContentChange(contentId, e.cdnUrl);
-  };
+    const handleChangeEvent = (e: {
+        cdnUrl: string | string[] | string[][];
+    }) => {
+        onContentChange(contentId, e.cdnUrl);
+    };
 
-  return (
-    <div>
-      <FileUploaderRegular
-        sourceList="local, url, dropbox"
-        classNameUploader="uc-light"
-        pubkey={process.env.UPLOADCARE_PUBLIC_KEY!}
-        multiple={false}
-        onFileUploadSuccess={handleChangeEvent}
-        maxLocalFileSizeBytes={10000000}
-      />
-    </div>
-  );
+    return (
+        <div>
+            <FileUploaderRegular
+                sourceList="local, url, dropbox"
+                classNameUploader="uc-light"
+                pubkey={process.env.UPLOADCARE_PUBLIC_KEY!}
+                multiple={false}
+                onFileUploadSuccess={handleChangeEvent}
+                maxLocalFileSizeBytes={10000000}
+            />
+        </div>
+    );
 }
 
 export default UploadImage;
